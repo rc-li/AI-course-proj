64e755c2d36e52b593ed7c1e6cb01b6ae0b7b201
rc-li <ruicheng.li@uconn.edu>
2019-10-18 00:57:31 -0700

    added the methods signatures
    and the action() pseudocode

--- halmaGame/src/halmaGame/homework.java
+++ halmaGame/src/halmaGame/homework.java
@@ -10,11 +10,13 @@ public class homework {
 		String colorUPlay;
 		float timeRemaining;
 		char[][] board = new char[16][16];
+		int eval_value;
 		
 	}
 	
 	public static void main(String args[]) throws FileNotFoundException {
 		State state = readInput();
+		System.out.println("hw2 terminated");
 	}
 	
 	private static State readInput() throws FileNotFoundException {
@@ -33,5 +35,44 @@ public class homework {
 		}
 		s.close();
 		return state;
+	}
+	
+	private static State abSearch(State state) {
+		int v = maxValue(state, Integer.MIN_VALUE, Integer.MAX_VALUE);
+		return state;
+	}
+	
+//	For each piece, 
+//	For each direction
+//	if the adjacent is empty, 
+//		make a new state like that and add that to collection
+//		Evaluate it and put the eval value inside the state
+//	Else (if the adjacent is occupied)
+//	If the other side is empty
+//		Jump(state) 
+//
+//Jump(state, otherside)
+//	Make a new state of the other side that and add that to collection
+//	Evaluate it and put the eval value inside the state
+//	If (an adjacent spot that has not been jumped over is jumpable)
+//		Jump(new state, new otherside)
+	
+	private static State[] actions(State state) {
+		
+		return null;
+	}
+	
+	private static int maxValue(State state, int a, int b) {
+		int v = Integer.MAX_VALUE;
+		return v;
+	}
+	
+	private static int minValue(State state, int a, int b) {
+		int v = Integer.MAX_VALUE;
+		return v;
+	}
+	
+	private static void output() {
+		
 	}
 }
