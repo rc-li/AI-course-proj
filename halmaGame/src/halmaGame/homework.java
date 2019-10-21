a39e696f680cde103fe72215742c3ff94431833b
rc-li <ruicheng.li@uconn.edu>
2019-10-21 13:40:14 -0700

    getting a stackoverflow exception
    when implementing the recursive jump

--- halmaGame/src/halmaGame/homework.java
+++ halmaGame/src/halmaGame/homework.java
@@ -225,6 +225,37 @@ public class homework {
 			}
 		}
 		
+		if (state.jumpX == -1 && state.jumpY == -1) {
+			int[][] directions = {{1,1},{1,0},{1,-1},{0,1},{0,-1},{-1,1},{-1,0},{-1,-1}};
+			for (int[] direction : directions) {
+				neighborX = state.currentX + direction[0];
+				neighborY = state.currentY + direction[1];
+				jumpX = state.currentX + direction[0]*2;
+				jumpY = state.currentY + direction[1]*2;
+				if (0 <= jumpX && jumpX <= 16 && 0 <= jumpY && jumpY <= 16) {
+					if (state.board[jumpY][jumpX] == '.' && state.board[neighborY][neighborX]!='.') {
+						State newState = (State) state.clone();
+						newState.board[jumpY][jumpX] = state.board[currentY][currentX];
+						newState.board[currentY][currentX] = '.';
+						int[] yourNewMinion = {jumpX,jumpY};
+						newState.yourMinions.set(minionExamined, yourNewMinion);
+						newState.currentX = jumpX;
+						newState.currentY = jumpY;
+						newState.neighborX = -1;
+						newState.neighborY = -1;
+						newState.jumpX = -1;
+						newState.jumpY = -1;
+						states.add(newState);
+						
+						System.out.println("jumped from "+currentX+","+currentY+" to "+jumpX+","+jumpY);
+						jumps++;
+						
+						jump(newState, jumpX, jumpY, states);
+					}
+				}
+			}
+		}
+		
 	}
 	
 	private static void printBoard(State state) {
